897eea9c21df1b02f035f51cf3e2c843113b39aa
SebastienBoisard <sebastien.boisard@gmail.com>
2017-06-09 19:36:52 +0200

    Change testing framework for pytest   - modify travis config   - simplify tests thanks to pytest

--- test_prepare_data.py
+++ test_prepare_data.py
@@ -1,28 +1,21 @@
-import unittest
 import prepare_data
 import io
 
 
-class TestPrepareData(unittest.TestCase):
+def test_extract_article_and_abstract():
+    initial_text = "".join([
+        "Chelsea are waiting on the fitness of John Terry ahead of Friday's Champions League match with Valencia.",
+        '\n', 'John Terry tries out his protective mask during training for Chelsea on Tuesday.\n',
+        '\n',
+        '\n', '@highlight\n', '\n',
+        'The central defender underwent surgery on a broken cheekbone on Sunday'])
 
-    def test_extract_article_and_abstract(self):
-        initial_text = "".join([
-            "Chelsea are waiting on the fitness of John Terry ahead of Wednesday's Champions League match with Valencia.",
-            '\n', 'John Terry tries out his protective mask during training for Chelsea on Tuesday.\n',
-            '\n',
-            '\n', '@highlight\n', '\n',
-            'The central defender underwent surgery on a broken cheekbone on Sunday'])
+    expected_article = "chelsea are waiting on the fitness of john terry ahead of friday 's champions league match with valencia . " \
+        "john terry tries out his protective mask during training for chelsea on tuesday ."
+    expected_abstract = '<s> the central defender underwent surgery on a broken cheekbone on sunday . </s>'
 
-        expected_article = "chelsea are waiting on the fitness of john terry ahead of wednesday 's champions league match with valencia . " \
-            "john terry tries out his protective mask during training for chelsea on tuesday ."
-        expected_abstract = '<s> the central defender underwent surgery on a broken cheekbone on sunday . </s>'
+    story = io.BytesIO(initial_text.encode())
+    article, abstract = prepare_data.extract_article_and_abstract(story)
 
-        story = io.BytesIO(initial_text.encode())
-        article, abstract = prepare_data.extract_article_and_abstract(story)
-
-        self.assertEqual(expected_article, article, "Expected `{}`, got `{}`".format(expected_article, article))
-        self.assertEqual(expected_abstract, abstract, "Expected `{}`, got `{}`".format(expected_abstract, abstract))
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert article == expected_article
+    assert abstract == expected_abstract
--- test_vocabulary.py
+++ test_vocabulary.py
@@ -1,17 +1,10 @@
-import unittest
 from vocabulary import Vocabulary
 import io
 
 
-class TestVocabulary(unittest.TestCase):
+def test_load_vocab():
+    vocab = Vocabulary()
+    vocab_file_stream = io.StringIO("word 2\ntest 4")
+    vocab.load_vocab_from_stream(vocab_file_stream, 10)
 
-    def test_load_vocab(self):
-        vocab = Vocabulary()
-        vocab_file_stream = io.StringIO("word 2\ntest 4")
-        vocab.load_vocab_from_stream(vocab_file_stream, 10)
-
-        self.assertEqual(2, vocab.get_vocab_size(), "Expected vocab size = {}, got {}".format(1, vocab.get_vocab_size()))
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert vocab.get_vocab_size() == 2
